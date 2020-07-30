6dd7cc74786398719a5288cdba5f2fb03bdf07e7
Eugene Husnullin <eugenehusnullin@gmail.com>
2020-07-31 01:32:07 +0300

    persist value between renders with useRef in React Function Components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
   const [name, setName] = useState('')
-  const inputRef = useRef()
-
-  function focus() {
-    inputRef.current.focus()
-  }
+  const prevName = useRef('')
 
   useEffect(() => {
-    inputRef.current.focus()
-  }, [])
+    prevName.current = name
+  }, [name])
+
 
   return (
     <>
-      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
-      <div>My name is {name}</div>
-      <button onClick={focus}>Focus</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <div>My name is {name} and it used to be {prevName.current}</div>
     </>
   );
 }
